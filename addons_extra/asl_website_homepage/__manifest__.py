eee1a000a693aee9a2ad051b185bef1fa7568c3f
José Márquez Doblas <jose.marquez@atlantyasoftware.com>
2020-03-14 17:15:27 +0100

    First Commit

---
+++ addons_extra/asl_website_homepage/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.
+{
+    'name': 'ASL Website Homepage',
+    'version': '12.0.0.0',
+    'category': 'Website',
+    'author':  'Aula Software Libre',
+    'license': 'AGPL-3',
+    'summary': "",
+    'depends': [
+        'base',
+        'website',
+    ],
+    'data': [
+        'templates/homepage.xml',
+    ],
+    'demo': [
+
+    ],
+    'installable': True,
+}
